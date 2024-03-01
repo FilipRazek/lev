aedae64d3a9e1ea65b2125824bead2eb8e80ca97
Filip Razek <filip@razek.net>
2024-03-01 20:57:25 +0100

    :recycle: Simplify calculation logic

--- main.lisp
+++ main.lisp
@@ -10,13 +10,14 @@
   (let ((m (length a)) (n (length b)))
     (let ((v0 (array-0-n n))
           (v1 (make-array (1+ n) :initial-element 0)))
+      (replace v1 v0)
       (loop for i from 0 to (1- m) do
-        (setf (aref v1 0) (1+ i))
         (loop for j from 0 to (1- n) do
           (let ((v0j (aref v0 j)))
             (let ((deletion-cost (1+ v0j))
                   (insertion-cost (1+ (aref v1 j)))
                   (substitution-cost (+ v0j (bool->int (char/= (aref a i) (aref b j))))                                       ))
-              (setf (aref v1 (1+ j)) (min deletion-cost insertion-cost substitution-cost)))))
+              (let ((minimal-cost (min deletion-cost insertion-cost substitution-cost)))
+                (setf (aref v1 (1+ j)) minimal-cost)))))
         (rotatef v0 v1))
       (aref v0 n))))
