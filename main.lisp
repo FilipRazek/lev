d3bdf780dd0ec02ba7eed47174449288bcd2cc21
Filip Razek <filip@razek.net>
2024-03-03 12:42:40 +0100

    :recycle: Add v1j as loop variable

--- main.lisp
+++ main.lisp
@@ -12,9 +12,9 @@
           (v1 (make-array (1+ n) :initial-element 0)))
       (replace v1 v0)
       (loop for i from 0 below m do
-        (loop for j from 0 below n for v0j = (aref v0 j) do
+        (loop for j from 0 below n for v0j = (aref v0 j) and v1j = (aref v1 j) do
           (let ((deletion-cost (1+ v0j))
-                (insertion-cost (1+ (aref v1 j)))
+                (insertion-cost (1+ v1j))
                 (substitution-cost (+ v0j (bool->int (char/= (aref a i) (aref b j))))                                       ))
               (let ((minimal-cost (min deletion-cost insertion-cost substitution-cost)))
                 (setf (aref v1 (1+ j)) minimal-cost))))
